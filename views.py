8f7d6feb7f8446a38fa1a8a7414be5d6be0ec31e
cheylea <cheyleahopkinson@gmail.com>
2023-05-11 00:09:12 +0100

    Removed other pages to combine website into one page

--- views.py
+++ views.py
@@ -5,7 +5,3 @@ views = Blueprint(__name__,"views")
 @views.route("/")
 def home():
     return render_template("index.html")
-
-@views.route("/about")
-def about():
-    return render_template("about.html")
